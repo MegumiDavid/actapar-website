7d28d087d1b85e41b515b80d0e4a3b1bb641f8ca
David Megumi Shinjo <dvdshinjo@gmail.com>
2022-07-09 20:54:12 -0300

    ajustes mobile view

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 const btnHamburger = document.querySelector('#btnHamburger')
 const menu = document.querySelector('.menu')
 
-btnHamburger.addEventListener('click', () => {
+btnHamburger.addEventListener('click', (e) => {
+    e.preventDefault()
     btnHamburger.classList.toggle('open-btn')
     menu.classList.toggle('open-menu')
 })

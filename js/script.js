38d307a426e63b0cb39b2847210ab73fe44cd9dd
David Megumi Shinjo <dvdshinjo@gmail.com>
2022-08-09 16:42:29 -0300

    traducao / cards home 😃👍

--- js/script.js
+++ js/script.js
@@ -8,11 +8,6 @@ btnHamburger.addEventListener("click", (e) => {
   menu.classList.toggle("open-menu");
 });
 
-if( document.documentElement.getAttribute("lang") === 'en' ) {
-  // Redirect to En page
-  console.log('ENGLISH');
-}
-
 // Change Language
 // - Menu showing and hiding
 const btnChangeLang = document.querySelector("#changeLangBtn");
@@ -39,49 +34,3 @@ btnChangeLang.addEventListener("click", () => {
     hideLangMenu();
   }
 });
-
-
-
-
-// - Changing language
-/* const changeLangPt = document.querySelector("#change-lang-pt");
-const changeLangEn = document.querySelector("#change-lang-en");
-const enText = document.querySelectorAll(".en-text");
-const ptText = document.querySelectorAll(".pt-text");
-const langImg = document.querySelector(".lang__img");
-
-function removeHiddenClass() {
-  document.querySelectorAll(".hidden-text").forEach((ht) => {
-    if (ht.classList.contains("hidden-text")) {
-      ht.classList.remove("hidden-text");
-    }
-  });
-}
-
-function translateToEn() {
-  removeHiddenClass();
-  ptText.forEach((pt) => {
-    pt.classList.add("hidden-text");
-  });
-  document.documentElement.setAttribute("lang", "en");
-  langImg.setAttribute("src", "images/united-states (1).png");
-}
-
-function translateToPt() {
-  removeHiddenClass();
-  enText.forEach((en) => {
-    en.classList.add("hidden-text");
-  });
-  document.documentElement.setAttribute("lang", "pt-br");
-  langImg.setAttribute("src", "images/brazil (1).png");
-}
-
-changeLangEn.addEventListener("click", () => {
-  translateToEn();
-  hideLangMenu();
-});
-changeLangPt.addEventListener("click", () => {
-  translateToPt();
-  hideLangMenu();
-});
- */
